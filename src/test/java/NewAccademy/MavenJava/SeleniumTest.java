0e15db08908805e2d1fd93d65fc6b4420b0b3344
Jasim <jasimjamal@calmove.in>
2020-06-14 09:05:11 +0530

    first commit

---
+++ src/test/java/NewAccademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,11 @@
+package NewAccademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void Browser()
+	{
+		System.out.println("Enter Browser Name");
+	}
+}
