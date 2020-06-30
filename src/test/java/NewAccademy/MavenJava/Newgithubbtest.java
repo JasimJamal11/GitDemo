bf6a4222a2bd88c3285989557a697b1d449476fa
Jasim Jamal <jasim@cmtserver.com>
2020-06-30 11:03:35 +0530

    From Calmove

---
+++ src/test/java/NewAccademy/MavenJava/Newgithubbtest.java
@@ -0,0 +1,18 @@
+package NewAccademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class Newgithubbtest {
+
+	
+	@Test
+	public void Browser1()
+	{
+		System.out.println("Enter Browser Name1");
+	}
+	@Test
+	public void Browser2()
+	{
+		System.out.println("Enter Browser Name2");
+	}
+}
